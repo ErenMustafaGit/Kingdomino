c79b32a9927215ad9ff6e7c69839c3eab649762a
Eren Mustafa Ceylan <eren-mustafa.ceylan@etu.unistra.fr>
2021-12-18 22:44:30 +0100

    Renamed MainMenu2 into MainMenu

--- kingdomino/src/View/MainMenu.java
+++ kingdomino/src/View/MainMenu.java
@@ -4,6 +4,7 @@ import Utilities.IMGReader;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,13 +23,26 @@ public class MainMenu extends JPanel {
         setOpaque(false);
 
 
-        JLabel header = new JLabel("Kingdomino Menu");
+        JLabel header = new JLabel("MENU");
+        header.setFont(new Font("Algerian", Font.BOLD, 48));
+        Color mycolor = new Color(174,135,0);
+        header.setForeground(mycolor);
+
+
         header.setSize(MyWindow.getWidth(),700);
         header.setHorizontalAlignment(JLabel.CENTER);
         header.setVerticalAlignment(JLabel.CENTER);
 
 
-        JButton button2 = new JButton("Play");
+        JButton button2 = new JButton("PLAY");
+        button2.setFont(new Font("Algerian", Font.BOLD, 30));
+       // mycolor = new Color(255,228,54);
+        button2.setBackground(mycolor);
+       // button2.setForeground(Color.black);
+        button2.setBorder(BorderFactory.createLineBorder(mycolor, 5));
+        button2.setPreferredSize(new Dimension(60,60));
+        this.setBorder(new EmptyBorder(10, 550, 200, 550));
+
         button2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -39,14 +53,16 @@ public class MainMenu extends JPanel {
 
 
         this.setLayout( new BorderLayout() );
-        this.add( header, BorderLayout.NORTH );
+        this.add( header, BorderLayout.PAGE_START );
 
-        JPanel center = new JPanel();
-        center.setOpaque(false);
-        center.setLayout( new GridLayout(10, 2) );
-        center.add(button2);
+        this.add(button2, BorderLayout.SOUTH);
 
-        this.add( center, BorderLayout.CENTER );
+//        JPanel center = new JPanel();
+//        center.setOpaque(false);
+//        center.setLayout( new GridLayout(10, 2) );
+//        center.add(button2, BorderLayout.PAGE_END);
+//
+//        this.add( center, BorderLayout.CENTER );
 
     }
 
