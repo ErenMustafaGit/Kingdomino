7083af68d833a4aeffc1066b6aa8b5c5008acd60
Eren Mustafa Ceylan <eren-mustafa.ceylan@etu.unistra.fr>
2021-12-19 19:29:25 +0100

    Set background IMG in ChoosingMenu

--- kingdomino/src/Utilities/CSVReader.java
+++ kingdomino/src/Utilities/CSVReader.java
@@ -3,6 +3,7 @@ package Utilities;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,7 +14,19 @@ public class CSVReader {
 
     public static List<List<String>> read(String filename){
 
-        //ClassLoader.getSystemClassLoader().getResourceAsStream(filename)
+
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(ClassLoader.getSystemClassLoader().getResourceAsStream(filename)))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                records.add(Arrays.asList(values));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //ClassLoader.getSystemClassLoader().getResourceAsStream(filename)*
+        /*
         List<List<String>> records = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(ressourcePath + filename))) { //InputStreamReader au lieu de FileReader pour l'exe
             String line;
@@ -23,7 +36,7 @@ public class CSVReader {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
         return records;
     }
 }
