2da63289193564ff3899a83982d629c6186d552e
ihsan <ihsane.errami@etu.unistra.fr>
2021-12-28 00:52:35 +0100

    commit for avoiding conflicts

--- rendu2/kingdomino/src/Model/NormalMode.java
+++ rendu2/kingdomino/src/Model/NormalMode.java
@@ -1,169 +1,102 @@
 package Model;
 
 public class NormalMode implements GameMode {
-    Ground ground;
+   //private static int BOARD_SIZE = 5;
+    private Positionable[][] board;
+
     @Override
     public int calculateScore(PlayerBoard p) {
-        int neighbor=0;
-        int crown=0;
-        for(int i =0; i<p.BOARD_SIZE; i++){
-            for(int j=0; j<p.BOARD_SIZE;j++){
-                neighbor =calculateNeighbor(p, ground,i,j);
-                crown = calculateNbCrown(p, ground, i, j);
-            }
-        }
-        return neighbor;
+        int count =0;
+       for(int i =0; i<p.BOARD_SIZE; i++){
+           for(int j=0; j<p.BOARD_SIZE; j++){
+               //count += countcell(p, i,j);
+               count +=countcrown(p, i , j);
+               //mettre que la case est deja visité
+               System.out.println("nombre = "+count);
+           }
+       }
+       return count;
     }
+    public int countcell(PlayerBoard p, int x, int y){
+        //on récupère le plateau du joueur
+        board = p.getBoard();
+        GroundColor type;
+        int count = 1;
 
-    public int calculateNeighbor(PlayerBoard p, Ground g, int i, int j) {
-        GroundColor color;
-        int nbNeighbor =1;
-        /**Calcul le nombre de voisin par domaine**/
-        for (i = 0; i < p.BOARD_SIZE; i++) {
-            for (j = 0; j < p.BOARD_SIZE; j++) {
-                if (p.getPositionnable(i, j) != null) {
-                    if (j - 1 >= 0) {
-                        //si la case du dessus n'est pas vide
-                        if (p.getPositionnable(i, j - 1) != null) {
-                            /**On récupère la couleur de la case au dessus**/
-                            color = p.getPositionnable(i, j - 1).getColor();
-                            /**Si la couleur de la case suivane est la même que celle qu'on a **/
-                            if (color == p.getPositionnable(i, j).getColor() && !p.getPositionnable(i, j - 1).isCounted()) {
-                                g = (Ground) p.getPositionnable(i, j - 1);
-                                /**RECURSIVITE**/
-                                nbNeighbor +=1;
-                                p.getPositionnable(i, j - 1).setCounted(true);
-                                calculateNeighbor(p, g, i, j - 1);
-                            }
+        type = board[x][y].getColor();
+        if(board[x][y]!=null){
+            if(y-1 >= 0){
+                if(board[x][y-1] !=null){
+                    if( type == board[x][y-1].getColor()  ){
+                       count +=countcell(p,x, y-1);
+                    }
+                }
+            }
+            if(x+1 < p.BOARD_SIZE){
+                if(board[x+1][y] !=null){
+                    if( type == board[x+1][y].getColor()){
+                        count +=countcell(p,x+1, y);
+                    }
+                }
 
-                        }
+            }
+            if(x-1 >= 0){
+                if(board[x-1][y] !=null){
+                    if( type == board[x-1][y].getColor()){
+                        count +=countcell(p,x-1, y);
                     }
-                    if (j + 1 < p.BOARD_SIZE) {
-                        //si la case d'en bas n'est pas vide
-                        if (p.getPositionnable(i, j + 1) != null) {
-                            /**On récupère la couleur de la case de droite**/
-                            color = p.getPositionnable(i, j + 1).getColor();
-
-                            /**Si la couleur de la case suivane est la même que celle qu'on a **/
-                            if (color == p.getPositionnable(i, j).getColor() && !p.getPositionnable(i, j + 1).isCounted()) {
-                                g = (Ground) p.getPositionnable(i, j + 1);
-                                /**RECURSIVITE**/
-                                nbNeighbor +=1;
-                                p.getPositionnable(i, j + 1).setCounted(true);
-                                calculateNeighbor(p, g, i, j + 1);
-                            }
-                        }
-                    }
-                    if (i - 1 > 0) {
-                        //si la case de gauche est pas vide
-                        if (p.getPositionnable(i - 1, j) != null) {
-                            /**On récupère la couleur de la case de droite**/
-                            color = p.getPositionnable(i - 1, j).getColor();
-                            /**Si la couleur de la case suivane est la même que celle qu'on a **/
-                            if (color == p.getPositionnable(i, j).getColor() && !p.getPositionnable(i - 1, j).isCounted()) {
-                                g = (Ground) p.getPositionnable(i - 1, j);
-                                /**RECURSIVITE**/
-                                nbNeighbor +=1;
-                                p.getPositionnable(i - 1, j).setCounted(true);
-                                calculateNeighbor(p, g, i - 1, j);
-                            }
-                        }
-                    }
-                    if (i + 1 < p.BOARD_SIZE) {
-                        if (p.getPositionnable(i + 1, j) != null) {
-                            /**On récupère la couleur de la case de droite**/
-                            color = p.getPositionnable(i + 1, j).getColor();
-                            /**Si la couleur de la case suivante est la même que celle qu'on a **/
-                            if (color == p.getPositionnable(i, j).getColor() && !p.getPositionnable(i + 1, j).isCounted()) {
-                                g = (Ground) p.getPositionnable(i + 1, j);
-                                /**RECURSIVITE**/
-                                nbNeighbor +=1;
-                                p.getPositionnable(i + 1, j).setCounted(true);
-                                calculateNeighbor(p, g, i + 1, j);
-                            }
-                        }
+                }
+            }
+            if(y+1 < p.BOARD_SIZE){
+                if(board[x][y+1] !=null){
+                    if( type == board[x][y+1].getColor()){
+                        count +=countcell(p,x, y+1);
                     }
                 }
             }
         }
-        System.out.println("Nombre de voisin : "+ nbNeighbor);
-         return nbNeighbor;
+        return count;
     }
+    public int countcrown(PlayerBoard p, int x, int y){
+        board = p.getBoard();
+
+        Ground g = new Ground(board[x][y].getColor(), board[x][y-1].getCrown());
+        int crown = g.getCrownNumber();
+        int count = 1;
 
 
-    public int calculateNbCrown(PlayerBoard p, Ground g, int i , int j){
-        GroundColor color;
-        int nbCrown =0;
-        /**Calcul le nombre de voisin par domaine**/
-        for (i = 0; i < p.BOARD_SIZE; i++) {
-            for (j = 0; j < p.BOARD_SIZE; j++) {
-                if (p.getPositionnable(i, j) != null) {
-                    if (j - 1 >= 0) {
-                        //si la case du dessus n'est pas vide
-                        if (p.getPositionnable(i, j - 1) != null) {
-                            /**On récupère la couleur de la case au dessus**/
-                            color = p.getPositionnable(i, j - 1).getColor();
-                            /**Si la couleur de la case suivane est la même que celle qu'on a **/
-                            if (color == p.getPositionnable(i, j).getColor() && !p.getPositionnable(i, j - 1).isCounted()) {
-                                g = (Ground) p.getPositionnable(i, j - 1);
-                                /**RECURSIVITE**/
-                                nbCrown +=1;
-                                p.getPositionnable(i, j - 1).setCounted(true);
-                                calculateNbCrown(p, g, i, j - 1);
-                            }
-
-                        }
+        if(board[x][y]!=null){
+            if(y-1 >= 0){
+                if(board[x][y-1] !=null){
+                    if(crown>0){
+                        count +=countcrown(p,x, y-1);
                     }
-                    if (j + 1 < p.BOARD_SIZE) {
-                        //si la case d'en bas n'est pas vide
-                        if (p.getPositionnable(i, j + 1) != null) {
-                            /**On récupère la couleur de la case de droite**/
-                            color = p.getPositionnable(i, j + 1).getColor();
+                }
+            }
+            if(x+1 < p.BOARD_SIZE){
+                if(board[x+1][y] !=null){
+                    if( crown>0){
+                        count +=countcrown(p,x+1, y);
+                    }
+                }
 
-                            /**Si la couleur de la case suivane est la même que celle qu'on a **/
-                            if (color == p.getPositionnable(i, j).getColor() && !p.getPositionnable(i, j + 1).isCounted()) {
-                                g = (Ground) p.getPositionnable(i, j + 1);
-                                /**RECURSIVITE**/
-                                nbCrown +=1;
-                                p.getPositionnable(i, j + 1).setCounted(true);
-                                calculateNeighbor(p, g, i, j + 1);
-                            }
-                        }
+            }
+            if(x-1 >= 0){
+                if(board[x-1][y] !=null){
+                    if( crown>0){
+                        count +=countcrown(p,x-1, y);
                     }
-                    if (i - 1 > 0) {
-                        //si la case de gauche est pas vide
-                        if (p.getPositionnable(i - 1, j) != null) {
-                            /**On récupère la couleur de la case de droite**/
-                            color = p.getPositionnable(i - 1, j).getColor();
-                            /**Si la couleur de la case suivane est la même que celle qu'on a **/
-                            if (color == p.getPositionnable(i, j).getColor() && !p.getPositionnable(i - 1, j).isCounted()) {
-                                g = (Ground) p.getPositionnable(i - 1, j);
-                                /**RECURSIVITE**/
-                                nbCrown +=1;
-                                p.getPositionnable(i - 1, j).setCounted(true);
-                                calculateNbCrown(p, g, i - 1, j);
-                            }
-                        }
-                    }
-                    if (i + 1 < p.BOARD_SIZE) {
-                        if (p.getPositionnable(i + 1, j) != null) {
-                            /**On récupère la couleur de la case de droite**/
-                            color = p.getPositionnable(i + 1, j).getColor();
-                            /**Si la couleur de la case suivante est la même que celle qu'on a **/
-                            if (color == p.getPositionnable(i, j).getColor() && !p.getPositionnable(i + 1, j).isCounted()) {
-                                g = (Ground) p.getPositionnable(i + 1, j);
-                                /**RECURSIVITE**/
-                                nbCrown +=1;
-                                p.getPositionnable(i + 1, j).setCounted(true);
-                                calculateNbCrown(p, g, i + 1, j);
-                            }
-                        }
+                }
+            }
+            if(y+1 < p.BOARD_SIZE){
+                if(board[x][y+1] !=null){
+                    if( crown >0){
+                        count +=countcrown(p,x, y+1);
                     }
                 }
             }
         }
-        System.out.println("Nombre de couronne :" + nbCrown);
-        return nbCrown;
+        return count;
     }
 
     @Override
