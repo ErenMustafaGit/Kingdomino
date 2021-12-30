6a183177c66b15acb7091933567ae50a36410c4e
ihsan <ihsane.errami@etu.unistra.fr>
2021-12-31 00:00:18 +0100

    creation of Singleton Class : Fontreader allowing to use it for the font

--- rendu2/kingdomino/src/Utilities/FontReader.java
+++ rendu2/kingdomino/src/Utilities/FontReader.java
@@ -1,20 +1,33 @@
 package Utilities;
 
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.awt.Font;
 
 public class FontReader {
     private static FontReader instance;
     private FontReader(){
 
     }
-    public static FontReader getInstance(){
+    public static FontReader getInstance() throws IOException, FontFormatException {
 
-        return null;
-        //fichier du premier font
-        //fichier du second font
 
-        //Création 1
-        //créatino 2
-        //return
+        if(instance == null) {
+            /**Création des Fonts**/
+            //fichier du font algerian pour les boutons | boutton commencer |harmony et middle kingdom | CBO nbre de joueurs - ALGERIAN
+            Font algerian = Font.createFont(Font.TRUETYPE_FONT, new File("./kingdomino/font/algerian.ttf"));
+            //fichier du font pour label menu du choix | Label Nbre de joueurs | les messages d'erreurs - Showcard Gothic
+            Font bookmanold = Font.createFont(Font.TRUETYPE_FONT, new File("./kingdomino/font/bookman-old-style.ttf"));
+            //fichier du font label mode de jeu - Bookman Old Style
+            Font showcard = Font.createFont(Font.TRUETYPE_FONT, new File("./kingdomino/font/showcard-gothic.ttf"));
+
+            //enregistrer les Fonts
+            GraphicsEnvironment g = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            g.registerFont(algerian);
+            g.registerFont(bookmanold);
+            g.registerFont(showcard);
+        }
+        return instance;
     }
 }
