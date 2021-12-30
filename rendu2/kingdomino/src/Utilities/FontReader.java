ab20ff506728e0d71286c2fac44ce041b6bcc960
ihsan <ihsane.errami@etu.unistra.fr>
2021-12-30 22:25:01 +0100

    add new Class FontReader for reading fonts and use it in labels

---
+++ rendu2/kingdomino/src/Utilities/FontReader.java
@@ -0,0 +1,14 @@
+package Utilities;
+
+import java.io.File;
+
+public class FontReader {
+    private static FontReader instance;
+    private FontReader(){
+
+    }
+    public static FontReader getInstance(){
+
+        return null;
+    }
+}
